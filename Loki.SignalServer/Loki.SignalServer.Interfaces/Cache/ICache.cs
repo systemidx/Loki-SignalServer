5423acc72301b5c8e802ec98b635c497e122b989
Bryon Weber <bweber@connectionseducation.com>
2017-08-25 00:56:44 -0400

    Bumping nuget versions.

--- Loki.SignalServer/Loki.SignalServer.Interfaces/Cache/ICache.cs
+++ Loki.SignalServer/Loki.SignalServer.Interfaces/Cache/ICache.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Loki.SignalServer.Interfaces.Cache
 {
     public interface ICache
     {
+        /// <summary>
+        /// Gets the specified key.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
         T Get<T>(string key) where T : class;
+
+        /// <summary>
+        /// Searches the cache.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns></returns>
         T[] Search<T>(Func<T, bool> predicate) where T : class;
+
+        /// <summary>
+        /// Sets the specified key.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
         void Set<T>(string key, T value);
     }
 }
